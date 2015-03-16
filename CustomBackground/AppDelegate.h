df6c5944aeff4ea2e3545915fba60b6bc111018e
Andrew Madsen <andrew@mixedinkey.com>
2015-03-16 08:12:27 -0600

    Initial commit.

---
+++ CustomBackground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomBackground
+//
+//  Created by Andrew Madsen on 3/16/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomBackground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomBackground
+//
+//  Created by Andrew Madsen on 3/16/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
